1dd73da1be7771dcc24a08a334f3fd0195ae9bef
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2022-09-22 15:13:16 +0200

    Make c bindings from go lib build, compile and link

--- waku-sys/build.rs
+++ waku-sys/build.rs
@@ -1,1 +1,50 @@
-fn main() {}
+use bindgen;
+use std::env;
+use std::env::set_current_dir;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    let output_lib = "libgowaku.a";
+
+    set_current_dir("./vendor").unwrap();
+    Command::new("go")
+        .arg("build")
+        .arg("-buildmode=c-archive")
+        .arg("-o")
+        .arg(format!("./build/lib/{output_lib}"))
+        .arg("./library/")
+        .status()
+        .map_err(|e| println!("cargo:warning={}", e))
+        .unwrap();
+    set_current_dir("../").unwrap();
+
+    let mut lib_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    lib_dir.push("vendor");
+    lib_dir.push("build");
+    lib_dir.push("lib");
+
+    println!("cargo:rustc-link-search={}", lib_dir.display());
+    println!("cargo:rustc-link-lib=static=gowaku");
+    println!("cargo:rerun-if-changed=libgowaku.h");
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(format!("{}/{}", lib_dir.display(), "libgowaku.h"))
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
--- waku/src/lib.rs
+++ waku/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use waku_sys;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::ffi::CStr;
+    use std::os::raw::c_char;
+    use waku_sys::waku_content_topic;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn content_topic() {
+        let topic = unsafe {
+            waku_content_topic(
+                "foo_bar".as_ptr() as *mut c_char,
+                1,
+                "foo_topic".as_ptr() as *mut c_char,
+                "rfc26".as_ptr() as *mut c_char,
+            )
+        };
+
+        let topic_str = unsafe { CStr::from_ptr(topic) }
+            .to_str()
+            .expect("Decoded return");
+        println!("{}", topic_str);
     }
 }
