fa2f7c59687d6428942a496e75bd33629a0510b0
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2023-02-14 15:26:16 +0100

    Fix toychat example

--- examples/toy-chat/src/main.rs
+++ examples/toy-chat/src/main.rs
@@ -107,7 +107,7 @@ fn setup_node_handle() -> std::result::Result<WakuNodeHandle<Running>, Box<dyn E
     let node_handle = node_handle.start()?;
     for address in NODES.iter().map(|a| Multiaddr::from_str(a).unwrap()) {
         let peerid = node_handle.add_peer(&address, ProtocolId::Relay)?;
-        node_handle.connect_peer_with_id(peerid, None)?;
+        node_handle.connect_peer_with_id(&peerid, None)?;
     }
     node_handle.relay_subscribe(None)?;
     Ok(node_handle)
