44ad6804ae2823baeaa1d1ec8930afe01c9466da
Richard Ramos <info@richardramos.me>
2024-10-28 15:32:50 -0400

    fix: call waku_setup when instantiating waku_new

--- waku-bindings/src/node/management.rs
+++ waku-bindings/src/node/management.rs
@@ -15,8 +15,12 @@ use crate::utils::{get_trampoline, handle_json_response, handle_no_response, han
 /// Instantiates a Waku node
 /// as per the [specification](https://rfc.vac.dev/spec/36/#extern-char-waku_newchar-jsonconfig)
 pub fn waku_new(config: Option<WakuNodeConfig>) -> Result<WakuNodeContext> {
-    let config = config.unwrap_or_default();
+    unsafe {
+        waku_sys::waku_setup();
+    }
 
+
+    let config = config.unwrap_or_default();
     let config_ptr = CString::new(
         serde_json::to_string(&config)
             .expect("Serialization from properly built NodeConfig should never fail"),
