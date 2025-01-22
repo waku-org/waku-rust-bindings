a20f5fd8c94da6a281c50414da15ff14e524ce0d
Ivan Folgueira Bande <ivansete@status.im>
2025-01-22 20:56:08 +0100

    messagehash.rs only contains a String as the hex representation of msg hash

--- waku-bindings/src/general/messagehash.rs
+++ waku-bindings/src/general/messagehash.rs
@@ -1,40 +1,18 @@
 use crate::general::waku_decode::WakuDecode;
-use hex::FromHex;
 use serde::{Deserialize, Serialize};
-use std::convert::TryInto;
 use std::fmt;
-use std::fmt::Write;
 use std::hash::Hash;
 use std::str::FromStr;
 
 /// Waku message hash, hex encoded sha256 digest of the message
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone, Hash)]
-pub struct MessageHash([u8; 32]);
-
-impl MessageHash {
-    fn to_hex_string(&self) -> String {
-        self.0.iter().fold(String::new(), |mut output, b| {
-            let _ = write!(output, "{b:02X}");
-            output
-        })
-    }
-}
+pub struct MessageHash(String);
 
 impl FromStr for MessageHash {
     type Err = String;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        let s = s.strip_prefix("0x").unwrap_or(s);
-        // Decode the hexadecimal string to a Vec<u8>
-        // We expect a string format like: d38220de82fbcf2df865b680692fce98c36600fdd1d954b8a71e916dc4222b8e
-        let bytes = Vec::from_hex(s).map_err(|e| format!("Hex decode error MessageHash: {}", e))?;
-
-        // Ensure the length is exactly 32 bytes
-        let res = bytes
-            .try_into()
-            .map_err(|_| "Hex string must represent exactly 32 bytes".to_string())?;
-
-        Ok(MessageHash(res))
+        Ok(MessageHash(s.to_string()))
     }
 }
 
@@ -47,6 +25,6 @@ impl WakuDecode for MessageHash {
 // Implement the Display trait
 impl fmt::Display for MessageHash {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.to_hex_string())
+        write!(f, "{}", self.0)
     }
 }
