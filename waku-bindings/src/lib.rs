0bf7c2b9bd707670effd43b1fc44f2ae34c3d949
darshankabariya <darshan@status.im>
2026-03-02 16:51:18 +0530

    Revert "chore: align with logos-delivery"
    This reverts commit 8abc430b01d03d90ca8ac20cdb6e761da9e51a1e.

--- waku-bindings/src/lib.rs
+++ waku-bindings/src/lib.rs
@@ -8,6 +8,12 @@ pub mod node;
 // Re-export the LibwakuResponse type to make it accessible outside this module
 pub use general::libwaku_response::LibwakuResponse;
 
+// Required so functions inside libwaku can call RLN functions even if we
+// use it within the bindings functions
+#[allow(clippy::single_component_path_imports)]
+#[allow(unused)]
+use rln;
+
 pub use node::{
     waku_create_content_topic, waku_new, Initialized, Key, Multiaddr, PublicKey, RLNConfig,
     Running, SecretKey, WakuEvent, WakuMessageEvent, WakuNodeConfig, WakuNodeHandle,
