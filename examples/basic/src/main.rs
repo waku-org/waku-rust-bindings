18e5d89f7abfabcea003c7d2189fddb3ae63f51b
darshankabariya <darshan@status.im>
2026-03-02 16:51:32 +0530

    Revert "chore: align with logos-delivery"
    This reverts commit ef5252b8174106056578b230dd51641b0c7a4261.

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -46,9 +46,6 @@ async fn main() -> Result<(), Error> {
                     WakuEvent::ConnectionChange(_evt) => {
                         // dbg!("Conn change evt", evt);
                     }
-                    WakuEvent::NodeHealthChange(_evt) => {
-                        // dbg!("Node health change evt", evt);
-                    }
                     WakuEvent::Unrecognized(err) => panic!("Unrecognized waku event: {:?}", err),
                     _ => panic!("event case not expected"),
                 };
@@ -77,9 +74,6 @@ async fn main() -> Result<(), Error> {
                     }
                     WakuEvent::ConnectionChange(_evt) => {
                         // dbg!("Conn change evt", evt);
-                    }
-                    WakuEvent::NodeHealthChange(_evt) => {
-                        // dbg!("Node health change evt", evt);
                     }
                     WakuEvent::Unrecognized(err) => panic!("Unrecognized waku event: {:?}", err),
                     _ => panic!("event case not expected"),
--- waku-bindings/src/node/events.rs
+++ waku-bindings/src/node/events.rs
@@ -27,9 +27,6 @@ pub enum WakuEvent {
     #[serde(rename = "connection_change")]
     ConnectionChange(ConnectionChangeEvent),
 
-    #[serde(rename = "node_health_change")]
-    NodeHealthChange(NodeHealthChangeEvent),
-
     Unrecognized(serde_json::Value),
 }
 
@@ -63,14 +60,6 @@ pub struct ConnectionChangeEvent {
     pub peer_id: String,
     /// The message hash
     pub peer_event: String,
-}
-
-/// Type of `event` field for a `node health change` event
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct NodeHealthChangeEvent {
-    /// The node connection status: Disconnected, PartiallyConnected, or Connected
-    pub status: String,
 }
 
 #[cfg(test)]
